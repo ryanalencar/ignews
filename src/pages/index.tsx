1f4c5417f29bb00c3048a9c0993b99de405e7223
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-04 21:17:57 -0300

    feat: sass implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from "next";
 
+import styles from "../styles/home.module.scss";
+
 const Home: NextPage = () => {
-  return <h1>hello world</h1>;
+  return <h1 className={styles.title}>hello world</h1>;
 };
 
 export default Home;
