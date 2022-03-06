01a385c773d0ee0ca4148e7ea915ac5e08e7264c
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-06 15:02:48 -0300

    feat: home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import styles from '../styles/home.module.scss';
+
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>Início | ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1>hello world</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome </span>
+        <h1>
+          News about the
+          <span> React </span>
+          world.
+        </h1>
+        <p>
+          Get access to all the publications
+          <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="" />
+    </main>
   </>
 );
 
