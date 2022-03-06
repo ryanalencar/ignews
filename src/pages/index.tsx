745acb3490441cc756b3446360b866b329cf6c2d
ryanalencar <ryanalencarbarbosa1701@gmail.com>
2022-03-06 12:21:20 -0300

    feat: dynamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 import styles from "../styles/home.module.scss";
 
 const Home: NextPage = () => {
-  return <h1 className={styles.title}>hello world</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>hello world</h1>
+    </>
+  );
 };
 
 export default Home;
